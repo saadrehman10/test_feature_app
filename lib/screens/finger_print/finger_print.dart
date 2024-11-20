c42d8dbdddcd336b7197cf91c3f21456507bab2c
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-11-20 22:35:24 +0500

    update

--- lib/screens/finger_print/finger_print.dart
+++ lib/screens/finger_print/finger_print.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
 
 class FingerPrint extends StatefulWidget {
   const FingerPrint({super.key});
@@ -8,8 +9,40 @@ class FingerPrint extends StatefulWidget {
 }
 
 class _FingerPrintState extends State<FingerPrint> {
+  late final LocalAuthentication auth;
+  bool _supportState = false;
+
+  @override
+  void initState() {
+    super.initState();
+    auth = LocalAuthentication();
+    auth.isDeviceSupported().then((bool isSupported) => setState(() {
+          _supportState = isSupported;
+        }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          _supportState ? const Text('is supported') : const Text('is not supported'),
+          ElevatedButton(
+              onPressed: _getBiometricOptions,
+              child: const Text('get available biometric options')),
+        ],
+      )),
+    );
+  }
+
+  Future<void> _getBiometricOptions() async {
+    List<BiometricType> bioTypes = await auth.getAvailableBiometrics();
+    debugPrint(bioTypes.toString());
+    if (!mounted) {
+      return;
+    } else {
+      setState(() {});
+    }
   }
 }
